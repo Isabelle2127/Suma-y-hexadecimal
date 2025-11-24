a173e2c14e287423795d4d86c19d62c2067f0a63
Enibian Asto <isabele.asto@ulv.edu.mx>
2025-11-24 03:34:47 -0600

    Proyecto completo de las partes que me tocaron

---
+++ test/conversiones_operaciones_test.dart
@@ -0,0 +1,8 @@
+import 'package:conversiones_operaciones/conversiones_operaciones.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
